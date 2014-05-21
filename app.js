00abe68ad14dd4cb3325146a94ca0a05c63f9012
Sam Eubank <sameubank+zerious@gmail.com>
2014-05-20 20:11:45 -0700

    Adding beams to the project scripts

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-require('lighter')({env: 'dev'});
+require('lighter')({
+  env: 'dev',
+  scripts: [
+    'node_modules/lighter/node_modules/jymin/jymin.js',
+    'node_modules/lighter/node_modules/beams/beams-client.js',
+    'scripts'
+  ]
+});
