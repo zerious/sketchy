4e379e77ae1adb0d7ef7ea564a13185aa944953d
louisnk <whitelouis@gmail.com>
2014-05-25 01:40:36 -0700

    there's a board, it knows where you draw

--- scripts/sketchy.js
+++ scripts/sketchy.js
@@ -1,32 +1,91 @@
-var Sketchy = window.sketchy || {}
+var Sketchy = window.Sketchy || {}
 
 Sketchy.main = {
 
-	init: function() {
-		this.listen();
-		console.log(this);
-    alert('hello');
+  init: function() {
+    var self = this;
+
+    self.watchBoard(function(data) {
+      self.getJSON('test', data, self.drawLines);
+    });
+   
+    self._bindClickHandlers('')
+
+  },
+
+  grab: function(selector) {
+    var by = selector.slice(0,1);
+    var name = selector.slice(1);
     
-	},
+    select = {
+      '.': document.getElementsByClassName(name),
+      '#': document.getElementById(name),
+    }
 
-	_bindClickHandlers: function($el, target, action) {
-		var pointer = document.getElementByID('marker')
-	},
+    $el = select[by];
 
-	startSketch: function(x, y, callback) {
+    if (!$el) {
+      $el = document.getElementsByTagName(selector)
+    }
 
-	},
+    return($el);
+  },
 
+  _bindClickHandlers: function(selector, target, action, sketch) {
+    var $el = this.grab(selector);
 
+    $el.onclick = function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      action(this);
+    };
+  },
+
+  watchBoard: function(callback) {
+    var $board,start,path,end,shim;
+
+    $board = this.grab('#sketcher');
+    shim = {'x': $board.offsetLeft, 'y': $board.offsetTop }
+
+    $board.onmousedown = function(e) {
+      start = calcPosition(e);
+      path = [];
+
+      $board.onmousemove = function(e) {
+        var current = calcPosition(e);
+        path.push(current);
+      }
+    }
+
+    $board.onmouseup = function(e) {
+      end = calcPosition(e);
+
+      return callback({'start': start, 'path': path, 'end': end});
+    }
+
+    calcPosition = function(e) {
+      var x = e.clientX
+      var y = e.clientY
+
+      return {'x': x - shim.x, 'y': y - shim.y }
+    }
+  },
+
+  getJSON: function(url, data, callback) {
+    var request = new XMLHttpRequest();
+    request.onload = callback;
+    request.open('get',url,true);
+    request.send();
+  },
+
+  drawLines: function(data) {
+    console.log(data);
+    // TO DO: draw some lines?
+  },
 }
 
-window.onload(function() {
-	Sketchy.main.init();
-});
 
-
-
-console.log('Put your app code here');
+Sketchy.main.init();
 
 var beams = getBeams();
 
